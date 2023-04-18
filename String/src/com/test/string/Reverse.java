2faea63fd0c2686581c3089da69597825d7f28ff
sound <sound@LAPTOP-DES8LREQ>
2023-04-18 13:32:12 +0530

    Reverse word also added

--- String/src/com/test/string/Reverse.java
+++ String/src/com/test/string/Reverse.java
@@ -5,6 +5,13 @@ public class Reverse {
 		String s= "Java Selenium";
 				StringBuilder sb= new StringBuilder(s);
 				System.out.println(sb.reverse());
+				
+				//Word Reverse
+				
+				String a[] = s.split(" ");
+				for(int i=a.length-1;i>=0;i--) {
+					System.out.print(a[i]+" ");
+				}
 	}
 
 }
